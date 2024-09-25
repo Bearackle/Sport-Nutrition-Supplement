3fc40d7bee6ef280e8b29c0401bb4d3819ef0b37
huyleuit <22520549@gm.uit.edu.vn>
2024-09-25 22:06:55 +0700

    updating header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -4,10 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import SearchInput from "./SearchInput";
 import accountIcon from "/public/account-icon.webp";
-import addressIcon from "/public/address-icon.webp";
 import cartIcon from "/public/cart-icon.webp";
 import logo from "/public/logo.png";
-import phoneIcon from "/public/phone-icon.webp";
+import phoneIcon from "/public/phone-icon.svg";
 
 export const Header = () => {
   return (
@@ -15,47 +14,34 @@ export const Header = () => {
       style={{
         background:
           "linear-gradient(180deg, rgba(63,133,233,1) 0%, rgba(48,116,225,1) 50%, rgba(37,105,222,1) 100%)",
-        backgroundColor: "rgb(63, 133, 233)",
       }}
     >
-      <div className="mx-auto flex max-w-[75rem] flex-row items-center gap-6 py-2">
+      <div className="relative mx-auto flex h-[6rem] max-w-[75rem] flex-row items-center justify-around py-2">
+        <div className="absolute right-8 top-1 flex flex-row items-center gap-1">
+          <Image src={phoneIcon} alt="phone" className="size-[1rem]" />
+          <p className="text-center text-[0.75rem] font-semibold text-white">
+            Tel: 0333303802
+          </p>
+        </div>
         <Link href="/" className="flex flex-row items-center gap-2">
-          <Image src={logo} alt="logo" className="size-[4rem]" />
+          <Image src={logo} alt="logo" className="size-[4.5rem]" />
           <p
             className={cn(
               nunito.className,
-              "max-w-[12.5rem] text-center text-[0.6rem] font-extrabold uppercase !italic text-white",
+              "max-w-[15rem] text-center text-[0.7rem] font-extrabold uppercase text-white",
             )}
           >
             Thực phẩm dinh dưỡng thể thao sport nutrition supplement
           </p>
         </Link>
         <SearchInput />
-        <div className="flex flex-row items-center gap-2">
-          <Image src={phoneIcon} alt="" className="size-[1.75rem]" />
-          <div className="text-center text-[0.75rem] text-white">
-            <p>Gọi mua hàng</p>
-            <Link
-              href="tel:0333303802"
-              className="font-bold -tracking-[0.05rem]"
-            >
-              033 330 3802
-            </Link>
-          </div>
-        </div>
-        <div className="flex flex-row items-center gap-2">
-          <Image src={addressIcon} alt="" className="size-[1.75rem]" />
-          <p className="text-center text-[0.75rem] text-white">
-            Hệ thống cửa hàng
-          </p>
-        </div>
         <Link href="login" className="flex flex-row items-center gap-2">
           <Image src={accountIcon} alt="" className="size-[1.75rem]" />
-          <p className="text-center text-[0.75rem] font-semibold text-white">
+          <p className="text-center text-[0.875rem] font-semibold text-white">
             Đăng nhập
           </p>
         </Link>
-        <div className="">
+        <div>
           <button className="flex flex-row items-center gap-2 rounded-[0.625rem] border-[0.1rem] border-solid border-white px-3 py-2">
             <Image src={cartIcon} alt="" className="size-[1.75rem]" />
             <p className="text-center text-[0.875rem] font-semibold text-white">
--- frontend/src/components/header/SearchInput.tsx
+++ frontend/src/components/header/SearchInput.tsx
@@ -1,5 +1,7 @@
 "use client";
+import Image from "next/image";
 import React from "react";
+import searchIcon from "/public/search-icon.svg";
 
 export default function SearchInput() {
   const [searchQuery, setSearchQuery] = React.useState("");
@@ -8,14 +10,22 @@ export default function SearchInput() {
     e.preventDefault();
   };
   return (
-    <form action="" className="w-[25rem] rounded-[3.125rem] bg-white">
+    <form
+      action=""
+      className="flex w-[30rem] flex-row items-center rounded-[3.125rem] bg-white"
+    >
       <input
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Nhập tên sản phẩm..."
         className="grow bg-transparent px-4 py-2 text-[0.875rem] focus:outline-none"
       />
-      <button type="submit"></button>
+      <button
+        type="submit"
+        className="mr-[0.15625rem] flex size-8 items-center justify-center rounded-[6.25rem] bg-[#B5D4F5]"
+      >
+        <Image src={searchIcon} alt="search" className="size-4" />
+      </button>
     </form>
   );
 }
